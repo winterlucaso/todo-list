b1d0c8a68c7e3b5a4cf00537e06d2a3860ecc3ca
winterlucaso <58354096+winterlucaso@users.noreply.github.com>
2023-10-06 15:50:19 -0500

    fix base layout styling

--- src/application.js
+++ src/application.js
@@ -1,20 +1,24 @@
 function createHeader() {
     const header = document.createElement('header');
+    header.innerText = "header";
     return header;
 }
 
 function createNav() {
     const nav = document.createElement('nav');
+    nav.innerText = "nav";
     return nav;
 }
 
 function createMain() {
     const main = document.createElement('main');
+    main.innerText = "main";
     return main;
 }
 
 function createFooter() {
     const footer = document.createElement('footer');
+    footer.innerText = "footer";
     return footer;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  // resolve: {
-  //   extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss'],
-  //   modules: ['src', 'node_modules'] // Assuming that your files are inside the src dir
-  // },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss'],
+    modules: ['src', 'node_modules'] // Assuming that your files are inside the src dir
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: ['style-loader', 'css-loader'],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   mode: "development",
 };
